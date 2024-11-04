4e0299c8c45cf6d1889bbddb2c175875568c29b0
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-11-05 06:45:54 +0700

    fix contextDestroyed

--- src/main/java/com/fuem/listeners/ContextListener.java
+++ src/main/java/com/fuem/listeners/ContextListener.java
@@ -23,7 +23,6 @@ public class ContextListener implements ServletContextListener {
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        DataSourceWrapper.closeDataSource();
         System.out.println("Web server stopped!");
     }
 }
