e6aea35589ec87d995f50d273f030c2de372f771
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-11-05 08:39:54 +0700

    add logging for db configuration

--- src/main/java/com/fuem/utils/DataSourceWrapper.java
+++ src/main/java/com/fuem/utils/DataSourceWrapper.java
@@ -6,6 +6,8 @@ package com.fuem.utils;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,15 +18,26 @@ public class DataSourceWrapper {
     private static HikariDataSource dataSource;
     
     static {
-        HikariConfig config = new HikariConfig();
-        config.setDriverClassName(ConfigurationGetter.getProperty("db.driver"));
-        config.setJdbcUrl(ConfigurationGetter.getConnectionString());
-        config.setConnectionTimeout(20000);
-        config.setIdleTimeout(600000);
-        config.setMaxLifetime(1800000);
-        config.setMaximumPoolSize(10);
-        config.setMinimumIdle(5);
-        dataSource = new HikariDataSource(config);
+        try {
+            String driverClass = ConfigurationGetter.getProperty("db.driver");
+            System.out.println("driver " + driverClass);
+            Logger.getLogger(DataSourceWrapper.class.getName()).log(Level.CONFIG, driverClass);
+            String connectionString = ConfigurationGetter.getConnectionString();
+            System.out.println("connectioStr " + connectionString);
+            Logger.getLogger(DataSourceWrapper.class.getName()).log(Level.CONFIG, connectionString);
+            
+            HikariConfig config = new HikariConfig();
+            config.setDriverClassName(driverClass);
+            config.setJdbcUrl(connectionString);
+            config.setConnectionTimeout(20000);
+            config.setIdleTimeout(600000);
+            config.setMaxLifetime(1800000);
+            config.setMaximumPoolSize(10);
+            config.setMinimumIdle(5);
+            dataSource = new HikariDataSource(config);
+        } catch (Exception e) {
+            Logger.getLogger(DataSourceWrapper.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
     
     public static HikariDataSource getDataSource() {
